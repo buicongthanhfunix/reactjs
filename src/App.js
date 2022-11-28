cbb7440a6450c8f1730a1ffa76f5f957e4596390
congthanh <pkbao@tdcpts-MacBook-Pro.local>
2022-11-28 20:23:14 +0700

    Lab 03 - 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className='container'>
+          <NavbarBrand href='/'>CongTHanhQuan</NavbarBrand>
+        </div>
+      </Navbar>
+    <Menu />
+
+    </div>
+  );
+}
+
+export default App;
